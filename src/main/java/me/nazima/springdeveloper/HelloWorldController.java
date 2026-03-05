36bbb2c22e5161bb8ef7795770bd153fcd5754e1
Nazima <ergeshovanazima2@gmail.com>
2026-03-05 12:24:49 +0900

    init: Spring Boot project setup, controller 테스트

---
+++ src/main/java/me/nazima/springdeveloper/HelloWorldController.java
@@ -0,0 +1,28 @@
+package me.nazima.springdeveloper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+
+    }
+
+    //http://localhost/student?firstName=Sungchul&lastName=Park
+    @GetMapping("/student")
+    public Student getStudent(@RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName){
+        return new Student(firstName, lastName);
+    }
+
+    //http://localhost/student/ChanHo/Park
+    @GetMapping("/student/{firstName}/{lastName}")
+    public Student getStudent2(@PathVariable("firstName") String firstName, @PathVariable("lastName")String lastName){
+        return new Student(firstName, lastName);
+
+    }
+}
---
+++ src/main/java/me/nazima/springdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,10 @@
+package me.nazima.springdeveloper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
