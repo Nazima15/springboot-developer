972942f6c0b95c264378cbcc6cab3e54f7b539c2
Nazima <ergeshovanazima2@gmail.com>
2026-03-17 16:47:16 +0900

    feat: TestController와 TestService 구현 및 JUnit 테스트 코드 추가

--- src/main/java/me/nazima/springdeveloper/TestService.java
+++ src/main/java/me/nazima/springdeveloper/TestService.java
@@ -2,16 +2,19 @@ package me.nazima.springdeveloper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
-public class TestService  {
-    @Autowired
-    TestRepository memberRepository;
-    public List<Member> getAllMembers(){
-        return memberRepository.findAll(); //select * from member
+public class TestService {
 
+    @Autowired
+    public TestRepository memberRepository;
+
+    public List<Member> getAllMembers(){
+        return memberRepository.findAll(); //select * from member;
     }
 
+    public Member saveMember(Member member){
+        return memberRepository.save(member);
+    }
 }
